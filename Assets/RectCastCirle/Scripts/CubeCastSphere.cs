8ee3e8f42825628ad21933f41354c40fabedc651
qzl1994 <305165598@qq.com>
2017-01-18 12:01:08 +0800

    修改材质球，准备3D坐标系碰撞检测

---
+++ Assets/RectCastCirle/Scripts/CubeCastSphere.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeCastSphere : MonoBehaviour
+{
+    public Transform cube;
+    public Transform sphere;
+    public Button button;
+
+    public Vector3 AA;
+    public Vector3 CC;
+    public Vector3 O;
+    public float r;
+
+	void Start ()
+    {
+        button.onClick.AddListener(CheckHit);
+	}
+
+    public void CheckHit()
+    {
+
+    }
+}
